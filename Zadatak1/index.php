a005e8cf385035ae9d949a48fc5acce71ec599a1
MihajloESTS <milojevicm374@gmail.com>
2022-04-01 13:05:57 +0200

    first commit

---
+++ Zadatak1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Zadatak 1</h1>
+	<p>Osnovne aritmetičke operacije, tipovi podataka i ispis</p>
+	<?php 
+
+		$a = 5;
+		$b = 3;
+		echo "a: ";
+		var_dump($a);
+		echo "<br>";
+		echo "b: ";
+		var_dump($b);
+		echo "<br>";
+		echo "$a + $b = $a + $b  <br>"; 
+		echo "$a - $b = $a - $b  <br>"; 
+		echo "$a * $b = $a * $b  <br>"; 
+		echo "$a / $b = $a / $b  <br>"; 
+		echo "$a % $b = $a % $b  <br>"; 
+	?>
+</body>
+</html>
